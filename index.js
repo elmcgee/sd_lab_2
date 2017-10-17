40aa4d04919385bc8aba3c453175f85d8caae60c
jiyuhan <jiyu-han@uiowa.edu>
2017-10-17 16:49:29 -0500

    firefox security bug, wss instaed of ws

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if (!window.WebSocket) {
     //If the user's browser does not support WebSockets, give an alert message
     alert("Your browser does not support the WebSocket API!");
 } else {
-    socket = new WebSocket("ws://ec2-54-242-86-211.compute-1.amazonaws.com:8000/");
+    socket = new WebSocket("wss://ec2-54-242-86-211.compute-1.amazonaws.com:8000/");
     socket.onmessage = event => {
         console.log(event.data);
         $("#data_received").html("<p>" + event.data + "</p>");
