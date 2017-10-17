9044118f1302225337b48b33249f60272bfbffb1
jiyuhan <jiyu-han@uiowa.edu>
2017-10-17 16:21:32 -0500

    adding some implementations on the server side

---
+++ client/index.js
@@ -0,0 +1,17 @@
+var socket;
+//First test for the browsers support for WebSockets
+if (!window.WebSocket) {
+    //If the user's browser does not support WebSockets, give an alert message
+    alert("Your browser does not support the WebSocket API!");
+} else {
+    socket = new WebSocket("ws://ec2-54-242-86-211.compute-1.amazonaws.com:8000/");
+    socket.onmessage = event => {
+        console.log(event.data);
+        $("#received").html(event.data);
+    }
+}
+
+var formSubmit = function() {
+    var dataToSend = document.getElementById("sent").value;
+    socket.send(dataToSend);
+}
--- node/server.js
+++ node/server.js
@@ -1,20 +1,20 @@
 const WebSocket = require('ws');
-const wss1 = new WebSocket.Server({ port: 8000});
+const wss = new WebSocket.Server({ port: 8000});
 
 var clients = [];
 
+
 wss.on('connection', function(ws) {
-    clients.push(ws);
     ws.on('message', function(message) {
         console.log('received: %s', message);
         sendButSender(message, ws);
     });
-    ws.send("NEW USER JOINED");
 });
 
 function sendButSender (message, ws) {
-    for (var i = 0; i < clients.length; i++) {
-        if(ws !== clients[i])
-            clients[i].send("Message: " + message);
-    }
+    wss.clients.forEach(function(client) {
+        if(client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(message);
+        }
+    });
 }
